d4b03d6c9b568be9ebff70d92d4b339442e0e189
kxxt <rsworktech@outlook.com>
2022-02-26 08:48:12 +0800

    Finish exercise 10-2-insertionSort.

--- 10-2-insertionSort/src/main.cpp
+++ 10-2-insertionSort/src/main.cpp
@@ -1,8 +1,30 @@
-#include <iostream>
 #include "console.h"
+#include "vector.h"
+#include <algorithm>
+#include <iostream>
 using namespace std;
 
+Vector<int> &insertionSort(Vector<int> &vec);
+
 int main() {
-    cout << "Hello, world!" << endl;
-    return 0;
+  Vector<int> a{56, 25, 37, 58, 95, 19, 73, 30};
+  cout << insertionSort(a) << endl;
+  return 0;
+}
+
+Vector<int> &insertionSort(Vector<int> &vec) {
+  int nOrdered = 1;
+  while (nOrdered < vec.size()) {
+    int next = vec[nOrdered], idx = 0;
+    while (idx < nOrdered && vec[idx] < next)
+      ++idx;
+    // idx points to the first element that is larger than next(or equals to
+    // next)
+    // swap the other elements to the right side
+    for (int i = nOrdered; i > idx; i--)
+      swap(vec[i], vec[i - 1]);
+    vec[idx] = next;
+    ++nOrdered;
+  }
+  return vec;
 }
