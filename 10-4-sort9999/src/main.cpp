3cb2c3d5d0c30c0400cc01a93c91922d1449c337
kxxt <rsworktech@outlook.com>
2022-02-26 10:21:19 +0800

    Finish exercise 10-4-sort9999.

--- 10-4-sort9999/src/main.cpp
+++ 10-4-sort9999/src/main.cpp
@@ -1,8 +1,31 @@
-#include <iostream>
 #include "console.h"
+#include "vector.h"
+#include <iostream>
 using namespace std;
 
+const int MAX_BOUND = 10000;
+
+void countingSort(Vector<int> &vec);
+
 int main() {
-    cout << "Hello, world!" << endl;
-    return 0;
+  Vector<int> v{893, 2, 0, 1232, 2133, 23, 232, 88, 81, 121, 9887, 230, 9999};
+  countingSort(v);
+  cout << v << endl;
+  return 0;
+}
+
+// for integers in range[0, 9999]
+void countingSort(Vector<int> &vec) {
+  Vector<int> count(MAX_BOUND);
+  Vector<int> output(vec.size());
+  // count numbers
+  for (auto item : vec)
+    count[item] += 1;
+  // do a prefix sum
+  for (int i = 1; i < MAX_BOUND; i++)
+    count[i] += count[i - 1];
+  // place numbers in position
+  for (auto item : vec)
+    output[--count[item]] = item;
+  vec = output;
 }
