3dc53aeb3d205c79179a2b15100e9337b540ed3e
kxxt <rsworktech@outlook.com>
2022-02-26 08:17:54 +0800

    Finish exercise 10-1-raiseToPower.

--- 10-1-raiseToPower/src/main.cpp
+++ 10-1-raiseToPower/src/main.cpp
@@ -1,8 +1,38 @@
-#include <iostream>
 #include "console.h"
+#include <iostream>
+
+#define DEBUG
+
 using namespace std;
 
+double raiseToPower(double x, int n);
+
+#ifdef DEBUG
+static int counter = 0;
+#endif
+
 int main() {
-    cout << "Hello, world!" << endl;
-    return 0;
+  cout << fixed << raiseToPower(2.0, 32) << endl;
+#ifdef DEBUG
+  cout << counter << " method calls in total!" << endl;
+#endif
+  return 0;
+}
+
+double raiseToPower(double x, int n) {
+#ifdef DEBUG
+  counter++;
+#endif
+  if (n < 0)
+    throw "not implemented";
+  if (n == 0)
+    return 1;
+  if (n == 1) // Optional optimization for reducing method calls
+    return x;
+  double ans = raiseToPower(x, n / 2);
+  if (n % 2 == 0) {
+    return ans * ans;
+  } else {
+    return x * ans * ans;
+  }
 }
