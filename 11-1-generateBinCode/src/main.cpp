42bd1040b4841c473a56183dd993f90017620f92
kxxt <rsworktech@outlook.com>
2022-03-06 20:58:05 +0800

    Finish exercise 11-1-generateBinCode.

--- 11-1-generateBinCode/src/main.cpp
+++ 11-1-generateBinCode/src/main.cpp
@@ -1,8 +1,22 @@
-#include <iostream>
 #include "console.h"
+#include <iostream>
 using namespace std;
 
+void generateBinaryCodeHelper(const string &prefix, int nBits);
+
+void generateBinaryCode(int nBits);
 int main() {
-    cout << "Hello, world!" << endl;
-    return 0;
+  generateBinaryCode(3);
+  return 0;
+}
+
+void generateBinaryCode(int nBits) { generateBinaryCodeHelper("", nBits); }
+
+void generateBinaryCodeHelper(const string &prefix, int nBits) {
+  if (nBits == 0) {
+    cout << prefix << endl;
+    return;
+  }
+  generateBinaryCodeHelper(prefix + '0', nBits - 1);
+  generateBinaryCodeHelper(prefix + '1', nBits - 1);
 }
