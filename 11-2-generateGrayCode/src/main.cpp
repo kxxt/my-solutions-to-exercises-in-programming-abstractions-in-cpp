0f3503f2a7027a339ea381b1a2d0f1b9bc935a9e
kxxt <rsworktech@outlook.com>
2022-03-06 21:28:55 +0800

    Finish exercise 11-2-generateGrayCode.

--- 11-2-generateGrayCode/src/main.cpp
+++ 11-2-generateGrayCode/src/main.cpp
@@ -1,8 +1,42 @@
-#include <iostream>
 #include "console.h"
+#include <algorithm>
+#include <iostream>
+#include <vector>
 using namespace std;
 
+vector<string> generateGrayCode(int nBits);
+
+void generateGrayCodeHelper(int nBits, const string &prefix, bool reversed,
+                            vector<string> &ans);
+
+void printGrayCodes(vector<string> codes) {
+  for_each(codes.begin(), codes.end(),
+           [](auto const &code) { cout << code << endl; });
+}
+
 int main() {
-    cout << "Hello, world!" << endl;
-    return 0;
+  auto ans = generateGrayCode(3);
+  printGrayCodes(ans);
+  return 0;
+}
+
+vector<string> generateGrayCode(int nBits) {
+  vector<string> ans;
+  generateGrayCodeHelper(nBits, "", false, ans);
+  return ans;
+}
+
+void generateGrayCodeHelper(int nBits, const string &prefix, bool reversed,
+                            vector<string> &ans) {
+  if (nBits == 0) {
+    ans.push_back(prefix);
+    return;
+  }
+  if (!reversed) {
+    generateGrayCodeHelper(nBits - 1, prefix + '0', false, ans);
+    generateGrayCodeHelper(nBits - 1, prefix + '1', true, ans);
+  } else {
+    generateGrayCodeHelper(nBits - 1, prefix + '1', false, ans);
+    generateGrayCodeHelper(nBits - 1, prefix + '0', true, ans);
+  }
 }
